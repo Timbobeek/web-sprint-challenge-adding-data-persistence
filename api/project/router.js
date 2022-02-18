139c53f23ec35fa2a4d0b68b4fed11822d8fcc02
Tim Goloshchapov <tim.goloschapov@gmail.com>
2022-02-17 22:26:15 -0600

    getall api works

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,22 @@
-// build your `/api/projects` router here
+const router = require('express').Router()
+const Project = require('./model')
+
+
+router.get('/:project_id', (req,res,next)=>{
+  Project.getProjectById(req.params.project_id)
+    .then(resource => {
+      res.status(200).json(resource)
+    })
+    .catch(next)
+})
+
+
+router.use((err, req, res, next)=>{ // eslint-disable-line
+  res.status(500).json({
+    cstmMsg: 'something went wrong ;(',
+    message: err.message,
+    stack: err.stack
+  })
+})
+
+module.exports = router;
