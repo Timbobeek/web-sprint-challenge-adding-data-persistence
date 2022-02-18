854106050c147b9976e302e72db7fd5af8a1ca4c
Tim Goloshchapov <tim.goloschapov@gmail.com>
2022-02-18 02:08:17 -0600

    seed 02 easy part

--- data/seeds/02-make-projects.js
+++ data/seeds/02-make-projects.js
@@ -1,3 +1,43 @@
-exports.seed = function (knex){
-  
-}
+const projects = [
+  { project_name: "Finish Lambda School", project_completed: 1 },
+  {
+    project_name: "Find a job",
+    project_description:
+      "go through interviews, get offers, choose the best one and accept it",
+    project_completed: 1,
+  },
+  {
+    project_name: "Enjoy the rich life",
+    project_description:
+      "buy a Lambo, record an album, eat sushi everyday, travel the world",
+    project_completed: 0,
+  },
+];
+
+const resources = [
+  {resource_name: 'my brain', resource_description: 'a brain-shaped vacuum'},
+  {resource_name: 'my parents', resource_description: 'a very supportive couple of persons'},
+  {resource_name: 'patience'},
+  {resource_name: 'good health'},
+  {resource_name: 'lots of $$$$'},
+  {resource_name: 'free time'}
+]
+
+const tasks = [
+  // Finish Lambda School
+  {task_description: }
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+exports.seed = function (knex) {};
